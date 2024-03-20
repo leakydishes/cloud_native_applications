10b7b9e4897a75d851d94e1b18de0c8aac36e60b
leakydishes <79079577+leakydishes@users.noreply.github.com>
2024-03-21 00:38:18 +1100

    [week2]

---
+++ week2/server_get.js
@@ -0,0 +1,39 @@
+// Creates express application
+const express= require("express");
+    // what to return/ instance
+    const app = express();
+    // two ways to save a function
+        // function ()
+        // (parameters) => 
+
+    // Function
+    // inputs (n1 and n2) returns sum
+    const addTwoNumber= (n1,n2) => {
+        return n1+n2;
+    }
+    // two functions look at the end point
+    // GET request, calculates sum, returns json response
+    app.get("/addTwoNumber", (req,res)=>{
+        const n1= parseInt(req.query.n1); // query parameter
+        const n2=parseInt(req.query.n2);
+        const result = addTwoNumber(n1,n2);
+        res.json({statuscocde:200, data: result }); 
+    });
+
+    app.get("/", (req, res) => {
+        const n1 = "<html><body><H1>HELLO THERE </H1></body></html>";
+        res.set('Content-Type', 'text/html');
+        res.send(Buffer.from(n1));     
+    })
+    // calculates using function
+    console.log (addTwoNumber(19,12));
+    // port number
+    const port=3040;
+    // server, logs message
+    app.listen(port,()=> {
+        console.log("hello i'm listening to port "+port);
+    })
+    
+    // view in browser
+    // http://localhost:3040/addTwoNumber?n1=786&n2=787
+    // http://localhost:3040/ 
