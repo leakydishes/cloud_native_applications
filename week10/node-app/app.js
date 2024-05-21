151314332357824f6f2a8e039e3eb58da1dc79ac
leakydishes <79079577+leakydishes@users.noreply.github.com>
2024-05-22 00:20:57 +1000

    [week10]

--- week10/node-app/app.js
+++ week10/node-app/app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 8080;
 
 app.get('/', (req, res) => {
-  res.send('Hello - Deploying to Google Kubernetes Engine!');
+  res.send(`Hello, World!`);
+  res.send(`Version: 1.0.0\n`);
+  res.send(`Hostname: e9d361d8130f`);
 });
 
 app.listen(port, () => {
-  console.log(`App listening at http://localhost:${port}`);
+  console.log(`Hello, World! http://localhost:${port}`);
 });
